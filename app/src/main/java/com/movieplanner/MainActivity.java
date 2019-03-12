3c7a8775ca97f3ea857f45715ad9b0d5ee26e44e
Shrish Balasubramanian <b.shrish12@gmail.com>
2019-03-12 23:00:21 +1100

    call filehandler in main activity

--- app/src/main/java/com/movieplanner/MainActivity.java
+++ app/src/main/java/com/movieplanner/MainActivity.java
@@ -1,13 +1,21 @@
 package com.movieplanner;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.movieplanner.Handler.FileHandler;
+
 public class MainActivity extends AppCompatActivity {
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+
+        FileHandler h = new FileHandler();
+        h.parseMoviesFile(context);
     }
 }
