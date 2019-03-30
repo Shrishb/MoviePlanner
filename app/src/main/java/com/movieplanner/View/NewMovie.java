bef7055e1a4d0f351640ae22c54d3ceb52b0e1a8
Shrish Balasubramanian <b.shrish12@gmail.com>
2019-03-30 16:59:53 +1100

    make form and string changes

--- app/src/main/java/com/movieplanner/View/NewMovie.java
+++ app/src/main/java/com/movieplanner/View/NewMovie.java
@@ -2,15 +2,25 @@ package com.movieplanner.View;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.movieplanner.R;
 
 public class NewMovie extends AppCompatActivity {
+
+    ImageView addMoviePoster;
 
     //create new movie view
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_movie);
+
+        setImagePoster();
+    }
+
+    private void setImagePoster(){
+        addMoviePoster = (ImageView) findViewById(R.id.addMoviePoster);
+        addMoviePoster.setImageResource(R.drawable.poster);
     }
 }
